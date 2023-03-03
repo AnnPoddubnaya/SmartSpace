966ff3e00fe191860048ad1d7b82dbe268a866e5
Анна Поддубная <pod.dubnaya.anna1990@gmail>
2023-03-03 15:22:30 +0300

    add:block deals,adaptiv,tabs

--- js/index.js
+++ js/index.js
@@ -7,3 +7,33 @@ const swiper = new Swiper('.swiper', {
     },
 
 });
+
+//tabs
+
+const itemButtons = document.querySelectorAll('.tabs-deals__button');
+const itemTabs = document.querySelectorAll('.tabs-deals__items');
+
+
+document.addEventListener('click', (e) => {
+    let currentActiveItem = null;
+    let currentNewActive = null;
+    let targetElement = e.target;
+
+    if (targetElement.closest('.tabs-deals__button')) {
+        itemButtons.forEach((item, index) => {
+
+            if (item.classList.contains('active')) {
+                currentActiveItem = index;
+                item.classList.remove('active');
+            }
+
+            if (item === targetElement) {
+                currentNewActive = index;
+            }
+        })
+        targetElement.classList.add('active');
+        itemTabs[currentActiveItem].classList.remove('active');
+        itemTabs[currentNewActive].classList.add('active')
+
+    }
+});
